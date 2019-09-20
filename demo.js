f19f158f6a0adf75211c3deeffac918fb1c9775d
Nico De Witte <nico.dewitte@outlook.com>
2019-09-20 15:18:44 +0200

    Add greeter class

--- demo.js
+++ demo.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+// console.log(x);
+// var x = 15;
+// console.log(x);
+
+// // console.log(y); // Crashes
+// let y = 15;
+// console.log(y);
+
+// // Periodic
+// setInterval(() => {
+//     console.log("Hello");
+// }, 1000);
+
+// // 1-shot
+// setTimeout(() => {
+//     console.log("Bye");
+// }, 2000);
+
+const Greeter = require('./greeter');
+let greeter = new Greeter('Nico');
+console.log(greeter.greet());
